594e423c2484bd26f5dad9bab7dc20f754803cb3
Clement <masson.cle@gmail.com>
2014-04-24 14:51:23 +0200

    Ceci est un test :D

--- AISoccer/src/aisoccer/Sebbot.java
+++ AISoccer/src/aisoccer/Sebbot.java
@@ -25,7 +25,7 @@ import aisoccer.strategy.UniformCover;
 public class Sebbot
 {
 
-    /**
+    /** CEC EST UN TEST -------------------------------------------------------
      * This is the entry point of the application.
      * Launch the soccer client using command line:
      * 
