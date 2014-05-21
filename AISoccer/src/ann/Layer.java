ed5a896a957ac814e17557b9f5389bd11294ce69
Clement <masson.cle@gmail.com>
2014-05-21 16:22:59 +0200

    ANN terminés

--- AISoccer/src/ann/Layer.java
+++ AISoccer/src/ann/Layer.java
@@ -1,9 +1,17 @@
 package ann;
 
+import math.MathFunction;
 import aisoccer.InvalidArgumentException;
 
 public class Layer {
 	Neuron[] neurons;
+	
+	public Layer(float[][] weights, MathFunction f){
+		neurons = new Neuron[weights.length];
+		for(int i=0; i<neurons.length; i++){
+			neurons[i] = new Neuron(weights[i], f);
+		}
+	}
 	
 	public float[] eval(float[] input) {
 		float[] out = new float[neurons.length];
@@ -11,6 +19,7 @@ public class Layer {
 			try {
 				out[i] = neurons[i].eval(input);
 			} catch (InvalidArgumentException e) {
+				System.err.println("probleme de dimension au neurone "+i);
 				e.printStackTrace();
 			}
 		}
--- AISoccer/src/ann/Network.java
+++ AISoccer/src/ann/Network.java
@@ -1,19 +1,41 @@
 package ann;
 
+import java.util.ArrayList;
+
+import math.MathFunction;
 import aisoccer.InvalidArgumentException;
+
 
 public class Network {
 	Layer[] layers;
 	
+	public Network(ArrayList<float[][]> weights, MathFunction f) throws InvalidArgumentException{
+		for(int i=0; i<weights.size(); i++){
+			if(i>0){
+				if (weights.get(i-1).length+1 != weights.get(i)[1].length){
+					throw new InvalidArgumentException();
+				}
+				layers[i] = new Layer(weights.get(i), f);
+			}
+		}
+	}
+	
 	public float[] eval(float[] input) {
-		float[] out = new float[layers.length+1];
+		float[] out = null;
+		float[] in = expand(input);		
 		for(int i=0; i<layers.length; i++){
-			//try {
-				//out[i] = layers[i].eval(input);
-			//} catch (InvalidArgumentException e) {
-			//	e.printStackTrace();
-			//}
+			out = layers[i].eval(in);
+			in = expand(out);
 		}
 		return out;
+	}
+	
+	public float[] expand(float[] v){
+		float[] r = new float[v.length+1];
+		for(int i=0; i<v.length; i++){
+			r[i] = v[i];
+		}
+		r[r.length] = 1;
+		return r;
 	}
 }
