7c7747b8885759181eab1a4873298ec2d2a32a71
Jeremy Morvan <morvanjeremy@gmail.com>
2014-05-29 15:01:34 +0200

    with more matlab

--- AISoccer/MatlabCode/Script4.m
+++ AISoccer/MatlabCode/Script4.m
@@ -2,15 +2,16 @@ close all;
 clear all;
 clc;
 
-[X1,X2,T1,T2] = ExtractInfos('/bin/TrainingLogs.txt');
-[X,T] = shuffle(X1,X2,T1,T2);
-
-hidden = 15;
-Win = 0.1*randn(hidden,6);
-Vin = 0.1*randn(1,hidden+1);
-
-[W,V,errors] = delta2(X,T,Win,Vin,0.005,10000,0.7);
-
-save('ANNParams','W','V');
-
-WriteInFile('ANN-Intercepted.txt',W,V);
+[X1,X2,T1,T2] = ExtractInfos('TrainingLogs.txt');
+% [X,T] = shuffle(X1,X2,T1,T2);
+% 
+% hidden = 15;
+% Win = 0.1*randn(hidden,6);
+% Vin = 0.1*randn(1,hidden+1);
+% 
+% 
+% [W,V,errors] = delta2(X,T,Win,Vin,1/size(X,2),10000,0.7);
+% 
+% save('ANNParams','W','V');
+% 
+% WriteInFile('ANN-Intercepted.txt',W,V);
