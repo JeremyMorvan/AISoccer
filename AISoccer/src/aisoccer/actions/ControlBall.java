48cc1655620dcc08b4c2878786010dc24cf4b74e
Clement <masson.cle@gmail.com>
2014-06-02 17:31:58 +0200

    Push

--- AISoccer/src/aisoccer/actions/ControlBall.java
+++ AISoccer/src/aisoccer/actions/ControlBall.java
@@ -19,11 +19,12 @@ public class ControlBall extends ActionTask {
 		Vector2D ballP = brain.getFullstateInfo().getBall().getPosition();
 		Vector2D ballV = brain.getFullstateInfo().getBall().getVelocity();
 		
-		Vector2D deltaP = brain.getPlayer().getPosition().subtract(ballP);
-		double deplacement = deltaP.polarRadius()-(SoccerParams.PLAYER_SIZE+SoccerParams.BALL_SIZE)/2;
-		Vector2D deplacementWanted = deltaP.normalize().multiply(deplacement);
+//		Vector2D deltaP = brain.getPlayer().getPosition().subtract(ballP);
+//		double deplacement = deltaP.polarRadius()-(SoccerParams.PLAYER_SIZE+SoccerParams.BALL_SIZE)/2;
+//		Vector2D deplacementWanted = deltaP.normalize().multiply(deplacement);		
+//		Vector2D accNeeded = deplacementWanted.subtract(ballV);
 		
-		Vector2D accNeeded = deplacementWanted.subtract(ballV);
+		Vector2D accNeeded = ballV.multiply(-1);
 		Vector2D powerNeeded = accNeeded.multiply(1/brain.getEffectivePowerRate());
 		double angleKick = MathTools.normalizeAngle(accNeeded.polarAngle()-brain.getPlayer().getBodyDirection());
 //		System.out.println(brain.getPlayer().getUniformNumber() + " : I am going to control");
